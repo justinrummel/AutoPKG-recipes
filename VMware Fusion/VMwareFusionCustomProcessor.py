8e0ce910b96e604a8825cab4a4d4896188076be0
Justin Rummel <j@justinrummel.com>
2014-07-29 10:49:30 -0400

    spelling, point urlopen to vsus, print server info

--- VMware Fusion/VMwareFusionCustomProcessor.py
+++ VMware Fusion/VMwareFusionCustomProcessor.py
@@ -1,2 +1,13 @@
-""" coments go here
+#!/usr/bin/env python
+
+""" comments go here
 """
+
+import urllib2
+
+vsus = urllib2.urlopen("https://softwareupdate.vmware.com/cds/vmw-desktop/fusion.xml")
+print vsus.info()
+fusion = vsus.read()
+
+
+vsus.close()
