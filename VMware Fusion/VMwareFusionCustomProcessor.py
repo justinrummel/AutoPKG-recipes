d8e0323904901395cf93a9685169b60b177b869c
Justin Rummel <j@justinrummel.com>
2014-07-29 12:40:39 -0400

    get the latest "core" download xml file path
    will need to download, gunzip, then read to begin the next steps.

--- VMware Fusion/VMwareFusionCustomProcessor.py
+++ VMware Fusion/VMwareFusionCustomProcessor.py
@@ -3,11 +3,42 @@
 """ comments go here
 """
 
-import urllib2
+import urllib, urllib2
 
-vsus = urllib2.urlopen("https://softwareupdate.vmware.com/cds/vmw-desktop/fusion.xml")
-print vsus.info()
-fusion = vsus.read()
+from xml.etree import ElementTree
 
+url = 'https://softwareupdate.vmware.com/cds/vmw-desktop/fusion.xml'
+request = urllib2.Request(url)
+
+try:
+    vsus = urllib2.urlopen(request)
+except URLError, e:
+    print e.reason
+
+data = vsus.read()
+# print data
+
+try:
+    metaList = ElementTree.fromstring(data)
+except ExpatData:
+    print "Unable to parse XML data from string"
+
+versions = []
+for metadata in metaList:
+    version = metadata.find("version")
+    versions.append(version.text)
+
+versions.sort()
+latest = versions[-1]
+# print latest
+
+urls = []
+for metadata in metaList:
+    url = metadata.find("url")
+    urls.append(url.text)
+
+matching = [s for s in urls if latest in s]
+core = [s for s in matching if "core" in s]
+print core[0]
 
 vsus.close()
