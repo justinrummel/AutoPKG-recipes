25fb195be3c79a16bfe5d0c2a3ad1bbabcb2ff50
Justin Rummel <j@justinrummel.com>
2014-08-04 20:54:44 -0400

    Educational Python for new Processor
    New Processor for Bundle pkg to Flat pkg.  Can’t work for complicated
    PKGs, but simple ones like Silverlight this should work.

---
+++ Silverlight/FlatPkgFromBundle.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python
+#
+# Copyright 2014 Justin Rummel
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import shutil
+import subprocess
+from xml.etree import ElementTree
+
+from autopkglib import Processor, ProcessorError
+
+
+__all__ = ["FlatPkgFromBundle"]
+
+
+class FlatPkgFromBundle(Processor):
+    
+    # Modifies the Bundle installer pkg so that:
+    #
+    # The basic set of operations:
+    #
+    # 1) pax -rzf Archive.pax.gz
+    # 2) Reflatten the pkg with pkgutil --flatten
+    #
+    # Note that this will remove any signing that is present. This is actually
+    # OK; it should not prevent any distribution system (Munki, Casper, etc)
+    # from installing this package.
+    #
+    # Lots of code "borrowed" from AdobereaderRepackager.py by Greg Neagle
+    #
+
+    description = "Finds the Bundle.PKG from a pkg recipe, expands by pax -rzf, then flattens to a Flat PKG for automated deployment (like the Casper Suite)."
+    input_variables = {
+        "pkg_path": {
+            "required": True,
+            "description": "Path to a bundle PKG.",
+        },
+    }
+    output_variables = {
+        "flat_pkg_path": "Path to the repackaged package."
+    }
+
+    __doc__ = description
+
+#    def find_pkg(self, dir_path):
+#        '''Return path to the first package in dir_path'''
+#        for item in os.listdir(dir_path):
+#            if item.endswith(".pkg"):
+#                return os.path.join(dir_path, item)
+#        raise ProcessorError(
+#            "No package found in %s" % dir_path)
+
+    def expand(self, pkg_path, expand_dir):
+        '''Uses pax to expand a bundle package.'''
+
+        if os.path.isdir(expand_dir):
+            try:
+                shutil.rmtree(expand_dir)
+            except (OSError, IOError), err:
+                raise ProccessorError(
+                    "Can't remove %s: %s" % (expand_dir, err))
+        try:
+            subprocess.check_call(
+                ['/bin/pax', '-rzf', pkg_path+"/Contents/Archive.pax.gz"])
+        except subprocess.CalledProcessError, err:
+            raise ProcessorError("%s expanding %s" % (err, pkg_path))
+
+    def flatten(self, expanded_pkg, destination):
+        '''Flatten an expanded flat pkg'''
+        if os.path.exists(destination):
+            try:
+                os.unlink(destination)
+            except OSError, err:
+                raise ProccessorError(
+                    "Can't remove %s: %s" % (destination, err))
+        try:
+            subprocess.check_call(
+                ['/usr/sbin/pkgutil', '--flatten', expanded_pkg, destination])
+        except subprocess.CalledProcessError, err:
+            raise ProcessorError(
+                "%s flattening %s" % (err, expanded_pkg))
+
+    def main(self):
+        try:
+#            pkg = self.find_pkg(bundle_pkg_path)
+            expand_dir = os.path.join(
+                self.env["RECIPE_CACHE_DIR"], "NAME")
+            modified_pkg = os.path.join(
+                self.env["RECIPE_CACHE_DIR"], os.path.basename(pkg_path))
+            expanded_pkg = self.expand(pkg_path, expand_dir)
+#            self.modify_distribution(expanded_pkg)
+#            self.replace_app_preinstall(expanded_pkg)
+            self.flatten(expanded_pkg, modified_pkg)
+            self.env["pkg_path"] = modified_pkg
+
+        except BaseException, err:
+            raise ProcessorError(err)
+
+
+if __name__ == '__main__':
+    processor = FlatPkgFromBundle()
+    processor.execute_shell()
+        
